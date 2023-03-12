aba937674c5577712f4982b5bf76cdd92e0a39aa
wxd <wangxddg@163.com>
2023-03-12 23:56:18 +0800

    -m "commitoo ht"

--- gitweb/src/main/java/com/example/gitweb/GitwebApplication.java
+++ gitweb/src/main/java/com/example/gitweb/GitwebApplication.java
@@ -15,7 +15,7 @@ public class GitwebApplication {
         System.out.println("ddddddddddddddddd3");
         System.out.println("oooooooooooooooooo");
         System.out.println("oooooooooooooooooo");
-
+        System.out.println("hot-fix test");
     }
 
 }
