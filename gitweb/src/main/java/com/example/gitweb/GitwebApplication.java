517c798db7e1af9d6ab87ae144c75b1c0d442264
wxd <wangxddg@163.com>
2023-03-12 22:47:57 +0800

    -m "commit1"

---
+++ gitweb/src/main/java/com/example/gitweb/GitwebApplication.java
@@ -0,0 +1,19 @@
+package com.example.gitweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitwebApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(GitwebApplication.class, args);
+
+        System.out.println("ddddddddddddddddd1");
+
+
+
+    }
+
+}
---
+++ gitweb/src/test/java/com/example/gitweb/GitwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitwebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
