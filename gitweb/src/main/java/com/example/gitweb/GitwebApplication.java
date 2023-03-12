bc116b4039bbb6d6e41c01f6b256a9e66efbbee9
wxd <wangxddg@163.com>
2023-03-12 23:22:55 +0800

    -m "commitoo"

--- gitweb/src/main/java/com/example/gitweb/GitwebApplication.java
+++ gitweb/src/main/java/com/example/gitweb/GitwebApplication.java
@@ -14,6 +14,7 @@ public class GitwebApplication {
         System.out.println("ddddddddddddddddd2");
         System.out.println("ddddddddddddddddd3");
         System.out.println("oooooooooooooooooo");
+        System.out.println("oooooooooooooooooo");
 
     }
 
